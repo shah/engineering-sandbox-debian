acd8ef15931e4d0447d60079955a83885a377ec3
Shahid N. Shah <shahid@shah.org>
2020-07-06 17:21:23 -0400

    Install Hugo and Hugo Extended (hugox) together

--- setup.sh
+++ setup.sh
@@ -27,8 +27,11 @@ curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=$SANDBOX_CONF_H
 
 export HUGO_REPO="gohugoio/hugo"
 export HUGO_VERSION=`curl -s https://api.github.com/repos/${HUGO_REPO}/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
-title "Install the latest version ($HUGO_VERSION) of Hugo static site generator"
-curl -L $SANDBOX_CONF_HOME/bin/hugo https://github.com/${HUGO_REPO}/releases/download/${HUGO_VERSION}/hugo_${HUGO_VERSION:1}_Linux-64bit.tar.gz \
+title "Install the latest version ($HUGO_VERSION) of hugo and hugox (extended)"
+curl -L https://github.com/${HUGO_REPO}/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION:1}_Linux-64bit.tar.gz \
+     | tar -xz -C $SANDBOX_CONF_HOME/bin hugo
+mv $SANDBOX_CONF_HOME/bin/hugo $SANDBOX_CONF_HOME/bin/hugox
+curl -L https://github.com/${HUGO_REPO}/releases/download/${HUGO_VERSION}/hugo_${HUGO_VERSION:1}_Linux-64bit.tar.gz \
      | tar -xz -C $SANDBOX_CONF_HOME/bin hugo
 
 [[ -f $HOME/.zshrc ]] || ln -s $SANDBOX_CONF_HOME/zshrc $HOME/.zshrc
